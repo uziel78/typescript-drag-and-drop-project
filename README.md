b35d988fa3e47850deaa9cdd27d2ca3570d7d8ff
uziel78 <rune.husa78@gmail.com>
2023-03-28 17:21:46 +0200

    first code

---
+++ README.md
@@ -0,0 +1,4 @@
+Drag and drop project from Section 9:
+https://www.udemy.com/course/understanding-typescript/
+
+For this I've just used the Live Server extension when working with it.
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,30 @@
-console.log('test');
+// =================== ACCESSING ELEMENTS / RENDERING FORM ===================== //
+// ============================================================================= //
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    // when instance of class is instansiated, we render the form
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+    
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+  }
+}
+
+
+const prjInput = new ProjectInput();
